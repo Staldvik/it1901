9eee757f57bc19ee2a5f0f102b9f312e3b13be23
Håvard Olsen <haavard.b.olsen@gmail.com>
2017-09-21 11:41:36 +0200

    endring test

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      prosjekt: "festival"
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to IT1901</h2>
+          <h2>Welcome to IT1901 {this.state.prosjekt}</h2>
         </div>
         <p className="App-intro">
           Hello World
