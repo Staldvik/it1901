8488a565472d4bc06b1567c6eeb4dc12f1cdeb18
marose14 <marose42@gmail.com>
2017-09-26 22:00:54 +0200

    Merge remote-tracking branch 'refs/remotes/origin/master' into Marias-Branch
    # Conflicts:
    #	app/src/App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './uka.png';
 import './App.css';
 import Artist from './components/artist/Artist'
 import Concert from './components/concert/Concert'
 import Technician from './components/technician/Technician'
+import Scene from './components/scene/Scene'
+
+// Prøver å lage navbar
+import NavComponent from './components/navbar/navbar';
 
 class App extends Component {
   constructor(){
@@ -18,7 +22,8 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Arrangørsoftware for  {this.state.prosjekt}</h2>
+          <h2>Arrangørsoftware for {this.state.prosjekt}</h2>
+          <NavComponent />
         </div>
         <p className="App-intro">
           Hver div er en component
@@ -28,11 +33,11 @@ class App extends Component {
         Han ble kjent gjennom nettstedene YouTube og SoundCloud, der han publiserer sin musikk som samlet har over 400 millioner treff." popularity={10} albumSales={400} cost={50000} earlierConcerts={["test1", "test2"]}
         />
 
-        <Concert price={300} sales={150}
-        />
+        <Concert price={300} sales={150} genre="Rock" />
 
-        <Technician name="Tekniker1"
-        />
+        <Technician name="Tekniker1" />
+
+        <Scene name="Dødens Dal" capacity={1000} cost={7500} />
 
       </div>
     );
