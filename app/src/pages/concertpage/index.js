8ea1fa955f086cca9b705981a2892cad63dadf04
Åsmund <aasmund.staldvik@gmail.com>
2017-09-27 14:14:50 +0200

    Jobber med å vise concerts på concerts-siden, mest for å finne ut av databasen

--- app/src/pages/concertpage/index.js
+++ app/src/pages/concertpage/index.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import NavComponent from '../../components/navbar/navbar'
 import logo from '../../uka.png'
 import './style.css';
+import * as firebase from 'firebase';
+import Concert from '../../components/concert/Concert'
 
 export default class ConcertPage extends Component {
   // static propTypes = {}
   // static defaultProps = {}
   // state = {}
+  constructor() {
+    super();
+    this.state = {
+      concerts: [
+        {id: 1, price: 100, sales: 10, genre: "rock", },
+        {id: 1, price: 100, sales: 10, genre: "rock", }
+      ]
+    }
+  }
 
   render() {
     return (
@@ -20,6 +31,13 @@ export default class ConcertPage extends Component {
           Concerts
         </h1>
         <p> This is just to test showing all concerts stored in database </p>
+        <div className="concertsBody"> {
+          this.state.concerts.map((concert) => {
+            return (
+              <Concert price={concert.price} sales={concert.sales} key={concert.id} />
+            )
+          })
+        }</div>
       </div>
     );
   }
