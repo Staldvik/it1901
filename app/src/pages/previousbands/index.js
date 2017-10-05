d30af407c1eb86db5c4697cd137247ffa54d2d48
marose14 <marose42@gmail.com>
2017-10-05 11:17:26 +0200

    Leser inn teknikere fra databasen

--- app/src/pages/previousbands/index.js
+++ app/src/pages/previousbands/index.js
@@ -10,6 +10,28 @@ export default class PreviousBands extends Component {
   // static defaultProps = {}
   // state = {}
 
+  constructor() {
+    super();
+    this.state = {
+      technicians: [],
+    }
+
+  }
+
+  componentWillMount() {
+    var previousTechnicians = this.state.technicians;
+
+    database.ref('festival17').child('technicians').on('child_added', technicianSnapshot => {
+      var vals = technicianSnapshot.val();
+      previousTechnicians.push ({
+        name:vals.name,
+      })
+    this.setState({
+      technicians: previousTechnicians,
+    })
+  })
+
+  }
 
 
 
@@ -30,7 +52,21 @@ export default class PreviousBands extends Component {
         <div className="app">
           <NavComponent/>
 
-          <h1> TeknikerTest </h1>
+          <h1> Teknikere </h1>
+          <ul>
+          <li> Tekniker1 </li>
+          <li> Tekniker2 </li>
+          </ul>
+
+          <div className="techniciansBody">
+          {
+          this.state.technicians.map((technician) => {
+            return (
+              <Technician name={technician.name} />
+            )
+          })
+          }
+          </div>
         </div>
       )
     }
