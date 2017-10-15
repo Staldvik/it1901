66e19e4e4ce55b036546716f0c5d1998692275bf
Åsmund <aasmund.staldvik@gmail.com>
2017-10-15 22:37:59 +0200

    fortsetter og prøver

--- app/src/pages/search/index.js
+++ app/src/pages/search/index.js
@@ -4,7 +4,7 @@ import './style.css';
 import database from '../../database';
 import Concert from '../../components/concert/Concert';
 import Technician from '../../components/technician/Technician';
-import * as SpotifyWebApi from 'spotify-web-api-node';
+
 
 export default class Search extends Component {
   constructor() {
@@ -30,27 +30,39 @@ export default class Search extends Component {
         console.log(data);
       }) */
 
-      
-      var client_id = '88641e06b03f46d886b98db9c58e9935'; // Your client id
-      var client_secret = '8b9aa7488fb2456a98d4168dd4b5c2c4'; // Your secret
-      
-      // your application requests authorization
-      var authOptions = {
-        headers: {
-          'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-        },
-        form: {
-          grant_type: 'client_credentials'
-        },
-        json: true
-      };
-
-      var myRequest = new Request('https://accounts.spotify.com/api/token', authOptions);
-
-      fetch(myRequest).then(response => console.log(response))
-      
-      
+  var ClientOAuth2 = require('client-oauth2')
     
+  var spotifyAuth = new ClientOAuth2({
+      clientId: '88641e06b03f46d886b98db9c58e9935',
+      clientSecret: '8b9aa7488fb2456a98d4168dd4b5c2c4',
+      accessTokenUri: 'https://accounts.spotify.com/api/token',
+      redirectUri: 'http://localhost:3000/callback',
+  })    
+
+  console.log(spotifyAuth)
+
+  spotifyAuth.credentials.getToken({
+    body: {
+      'grant_type' : 'client_credentials'
+    },
+
+    header: {
+      Authorization : ('Basic ' + new Buffer('88641e06b03f46d886b98db9c58e9935' + ':' + '8b9aa7488fb2456a98d4168dd4b5c2c4').toString('base64'))
+    }
+  })
+  .then(function (user) {
+    console.log(user) //=> { accessToken: '...', tokenType: 'bearer', ... } 
+  })
+/* 
+    .withBodyParameters({
+      'grant_type' : 'client_credentials'
+    })
+    .withHeaders({
+      Authorization : ('Basic ' + new Buffer(this.getClientId() + ':' + this.getClientSecret()).toString('base64'))
+    })
+ */
+
+
   }
   
     render(){
