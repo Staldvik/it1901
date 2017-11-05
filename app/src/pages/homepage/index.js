e60b0317da60067d15655fd1621c9aa90049c3fc
matias <matiashoha@gmail.com>
2017-11-05 20:28:04 +0100

    added outline for program table

--- app/src/pages/homepage/index.js
+++ app/src/pages/homepage/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './style.css';
-import Concert from '../../components/concert/Concert'
+import ProgramDays from '../../components/programdays/ProgramDays'
 
 //firebase
 import database from '../../database'
@@ -10,20 +10,73 @@ export default class HomePage extends Component {
   // static propTypes = {}
   // static defaultProps = {}
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
+   
     this.state = {
+      days: [],
     }
+  }
+
+  componentWillMount() {
+    var prevDays = this.state.days;
+    
+        database.ref(this.props.state.festival).child('program').on('child_added', snap => {
+          var vals = snap.val();
+    
+          prevDays.push({
+            id: snap.key,
+            date:vals.date,
+          })
+    
+          this.setState({
+            days: prevDays,
+          })
+        })
+
+
   }
 
   render() {
     return (
-      <div>
-        <h1>HomePage</h1>
-        <h1>HomePage</h1>
-        <h1>HomePage</h1>
-        <h1>HomePage</h1>
+      <div id="frontPage">
+        <h1>Program</h1>
+        
+        <table>
+          <thead>
+                <tr>
+                  {this.state.days.map((day) => {
+                        return(<ProgramDays date={day.date}/>)
+                      })
+                  } 
+                </tr>
+          </thead>
+          
+          <tbody>
+                  {this.state.days.map((day) => {
+                        return(<tr> 
+                          <td>
+                          </td>
+                          <td>
+                          </td>
+                          <td>
+                          </td>
+                          <td>
+                          </td>
+                          <td>
+                          </td>
+                          </tr> )
+                      })
+                  } 
+                
+               
+            
+
+          </tbody>
+                    
+        </table>
+        
       </div>
     );
   }
